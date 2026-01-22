1c3dc9ac860bbef2fb8f26d91f8dfc6b247c5464
Dennis Irimu <lyricaldnyt@gmail.com>
2026-01-22 09:38:56 +0300

    Updated functionality and styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
-for(var i =1;i<value;i++){
-  value+=i;
+$(document).ready(function() {
+  $("#pingpong-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var value = parseInt($("#value").val(), 10);
+    var $results = $("#results");
+    $results.empty();
+
+    if (isNaN(value) || value < 1) {
+      $results.append("<li>Please enter a number greater than 0.</li>");
+      return;
+    }
+
+    for (var i = 1; i <= value; i++) {
+      var output;
+      if (i % 15 === 0) {
+        output = "pingpong";
+      } else if (i % 3 === 0) {
+        output = "ping";
+      } else if (i % 5 === 0) {
+        output = "pong";
+      } else {
+        output = i;
+      }
+      $results.append("<li>" + output + "</li>");
+    }
   });
 });
